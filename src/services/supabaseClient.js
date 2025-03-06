ebbe663de8365cdf3e1c6df94a0dd8ee9205af74
skev24 <gkeving24@gmail.com>
2025-03-06 11:43:48 -0600

    Changes in access to the BD

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,6 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://eiwjaznbqthyfjgkjyvx.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpd2phem5icXRoeWZqZ2tqeXZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1MDMxMjAsImV4cCI6MjA1NjA3OTEyMH0.di5wq0bZ6Aj49PYmCyi3PNttfLjqv0NWHmIbqWaQmkk";
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_KEY;
+
+if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
+    console.error("Faltan las variables de entorno para Supabase");
+  }
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
