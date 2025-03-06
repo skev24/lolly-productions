d5dfe0705053be998cbacb46b638109f2c8f1cf3
skev24 <gkeving24@gmail.com>
2025-03-06 11:48:42 -0600

    A minor change in supabaseClient

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -3,8 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
 const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_KEY;
 
-if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
-    console.error("Faltan las variables de entorno para Supabase");
-  }
-
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
